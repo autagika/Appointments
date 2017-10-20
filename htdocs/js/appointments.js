7a5c40899bf216a185990ff35efeb845e721500b
autagika <aputagikar@gmail.com>
2017-10-19 17:59:11 -0700

    Creating the Appointments Repository

---
+++ htdocs/js/appointments.js
@@ -0,0 +1,159 @@
+// Author: Ashish Utagikar
+// Date: 10/19/2017
+
+// This script contains the code for all the events handlers. The events are new , add, cancel and search. 
+// It also searches and displays the database records based on the search string
+
+
+//getAppointments function gets the appointments table from the database
+// when a search is done
+
+function getAppointments(appointments) {
+
+	var datastring = 'searchstring=' + appointments;
+
+	$.ajax({
+		dataType : 'json',
+		method : 'GET',
+		data : datastring,
+		url : 'search.cgi',
+
+		success : function(data) {
+			if (data) {
+				var len = data.length;
+				var txt = "";
+				txt += "<tr><td>" + "Date" + "</td><td>" + "Time" + "</td><td>"
+						+ "Description" + "</td></tr>";
+
+				for (var i = 0; i < len; i += 3) {
+
+					txt += "<tr><td>" + data[i] + "</td><td>" + data[i + 1]
+							+ "</td><td>" + data[i + 2] + "</td></tr>";
+
+				}
+
+				$("#table").append(txt);
+
+			}
+
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('error: ' + textStatus + ': ' + errorThrown);
+		}
+	});
+
+	return false;
+}
+
+$(document)
+		.ready(
+				function() {
+
+					$(window).load(function() {
+						
+						getAppointments("");
+					});
+
+					// Adds an appointment to the database after doing the client side validation
+					// if the validation fails then return false
+					$('#add')
+							.click(
+									function(e) {
+
+										$("#successmessage").empty();
+										$("#errorMessage").empty();								
+										
+										var appointmenttime = $(
+												'#datetimepicker').val();
+										var description = $('#description')
+												.val();
+										var error = 0;
+
+										
+
+										if (!$('#datetimepicker').val()) {
+
+											error = 1;
+
+											$("#errorMessage").append(
+													'Please enter the appointment time'
+															+ '<br /><br />');
+
+										}
+
+										if (!$("#description").val()) {
+
+											error = 1;
+											$("#errorMessage")
+													.append(
+															'<br />'
+																	+ 'Please enter the appointment description'
+																	+ '<br /><br />');
+
+										}
+
+										var currenttime = Date.now();
+										var d = $('#datetimepicker')
+												.datetimepicker('getValue');
+										var apt_time;
+										if (d !== null) {
+											apt_time = d.getTime();
+										}
+
+										if (currenttime > apt_time) {
+											error = 1;
+											$("#errorMessage")
+													.append(
+															'<br />'
+																	+ 'Cannot make appointments in the past. Please check Appointment date and time '
+																	+ '<br /><br />');
+										}
+
+										if (error === 1) {
+											
+											return false;
+										}
+										
+										return true;
+
+									});
+
+					// If the new button is clicked, show the appointment form and hide the new button
+					$("#new").click(function() {
+						$("#frontend").hide();
+						$("#apptform").show();
+					});
+
+					
+
+					// If the cancel button is clicked, hide the appointment form and show the new button
+					$("#cancel").click(function() {
+						
+						$("#errorMessage").empty();
+
+						$("#successmessage").empty();
+						
+						$("#frontend").show();
+						$("#apptform").hide();
+
+					});
+
+					
+					
+					// Show the Appointment widget
+					$('#datetimepicker').datetimepicker({});
+
+					// When the search button is clicked, search the database using the search string and return back the
+					// database records
+					$("#search").click(function() {
+						$("#table").empty();
+
+						var appointments = $('#appointments').val();
+
+						getAppointments(appointments);
+
+					});
+
+					
+
+				});
---
+++ htdocs/search.cgi
@@ -0,0 +1,100 @@
+#!C:\Perl64\bin\perl5.24.2
+
+# Author: Ashish Utagikar
+# Date: 10/19/2017
+
+# This script searches an appointment using the search
+# string in the description column and diplays the records
+# If the search string is empty then it displays all the records
+
+
+use strict;
+
+use warnings;
+use CGI;
+use DBI;
+use Time::Local;
+use JSON;
+use DateTime;
+
+$| = 1;
+
+my $CGI = new CGI();
+
+my $json_obj = new JSON;
+
+sub main {
+
+	print $CGI->header( -type => "application/json", -charset => "utf-8" );
+
+    # Get the search string from the client
+	my $search = $CGI->param("searchstring");
+
+    ## Connecting to the database
+	my $dbh = DBI->connect( "dbi:mysql:webapp", "autagika", "arizona" );
+
+    # Exit if it failed to connect to the database
+	unless ( defined($dbh) ) {
+		die "Cannot connect to the database.\n";
+	}
+
+	my $sth1;
+
+     # search the description column using the search string
+	if ($search) {
+		$sth1 = $dbh->prepare(
+
+qq{SELECT appointmenttime,Description FROM appointments WHERE Description LIKE ?}
+		);
+		my $result1 = $sth1->execute("%".$search."%");
+		
+		if (!$result1) {
+			die "An error occurred during the search. Please see the server log file";
+		}
+		
+	}
+	else {
+		$sth1 =
+		  $dbh->prepare(
+			qq{SELECT appointmenttime,Description FROM appointments});  ## if the search string is empty then get all the database records
+			 
+			my $result2 = $sth1 -> execute();
+			if (!$result2) {
+			die "An error occurred during the search. Please see the server log file";
+		}
+			
+	}
+	
+
+	
+	my @array = ();
+	
+	# Loop over all the database records and fetch them
+	
+	while ( my @ary = $sth1->fetchrow_array() ) {
+
+		my ( $date, $time ) = split /\s+/, $ary[0];
+				
+		my ( $hours, $minutes, $sec ) = split /\:/, $time;
+
+		my $hoursmin = $hours . ":" . "$minutes";
+						
+		push( @array, $date, $hoursmin, $ary[1] );
+
+	}
+
+    ## Write the database data as the JSON object
+    
+	print $json_obj->pretty->encode( \@array );
+
+	
+
+	$sth1->finish();
+
+	
+
+	$dbh->disconnect();
+
+}
+
+main();
